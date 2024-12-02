ce975671a72458cb9f2ffe1a0426338e2544b7e9
rbndm <rubendopico@gmail.com>
2024-12-02 23:57:05 +0100

    Added code to main class

--- src/main/java/com/dmrdmrdmr/learn/jbrains/tddintro/Main.java
+++ src/main/java/com/dmrdmrdmr/learn/jbrains/tddintro/Main.java
@@ -1,5 +1,17 @@
 package com.dmrdmrdmr.learn.jbrains.tddintro;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+
+        BarcodeEventProcessor barcodeEventProcessor = new BarcodeEventProcessor(
+                new ProductBarcodeService(List.of(new Product("100000001", "$1,00"))),
+                new BarcodePriceSenderService());
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        reader.lines().forEach(barcodeEventProcessor::onBarcode);
+    }
 }
