e74efad5d721c283daf5af6fd9d6fb9811b6f1d6
rbndm <rubendopico@gmail.com>
2024-12-02 19:59:17 +0100

    Solved mess reintegrating newflow branch

--- src/main/java/com/dmrdmrdmr/learn/jbrains/tddintro/BarcodeEventProcessor.java
+++ src/main/java/com/dmrdmrdmr/learn/jbrains/tddintro/BarcodeEventProcessor.java
@@ -1,10 +1,5 @@
 package com.dmrdmrdmr.learn.jbrains.tddintro;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,19 +11,10 @@ public class BarcodeEventProcessor {
 
     private final List<Product> products;
 
-    private final HttpClient httpClient;
-
     private String postedMessage;
 
-
-
     public BarcodeEventProcessor(List<Product> products) {
-        this(products, HttpClient.newBuilder().build());
-    }
-
-    public BarcodeEventProcessor(List<Product> products, HttpClient httpClient) {
         this.products = products;
-        this.httpClient = httpClient;
     }
 
 
@@ -41,37 +27,13 @@ public class BarcodeEventProcessor {
                 productPriceMsg = products.stream()
                         .filter(p -> barcode.equals(p.barcode()))
                         .findFirst()
-                        .map(p -> Optional.ofNullable(p.price())
-                                .map(this::sendPriceToServer)
-                                .orElse(MSG_PRICE_NOT_SET))
+                        .map(p -> Optional.ofNullable(p.price()).orElse(MSG_PRICE_NOT_SET))
                         .orElse(MSG_PRODUCT_NOT_FOUND);
             } else {
                 productPriceMsg = MSG_PRODUCT_NOT_FOUND;
             }
         }
         return productPriceMsg;
-    }
-
-    private String sendPriceToServer(String price) {
-
-        try {
-            HttpResponse<String> response = httpClient.send(
-                    HttpRequest.newBuilder()
-                            .POST(HttpRequest.BodyPublishers.ofString("text=" + price))
-                            .uri(URI.create("http://localhost:9393/display"))
-                            .build(),
-                    HttpResponse.BodyHandlers.ofString());
-
-            if(response.statusCode() == 200) {
-                return price;
-            } else {
-                return postedMessage;
-            }
-
-        } catch (IOException | InterruptedException e) {
-            return postedMessage;
-        }
-
     }
 
     public String getPostedMessage() {
