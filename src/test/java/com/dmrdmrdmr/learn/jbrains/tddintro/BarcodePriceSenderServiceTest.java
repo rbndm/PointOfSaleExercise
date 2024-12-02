6d73102cdd7cdef304383054e367ff3c4d657d36
rbndm <rubendopico@gmail.com>
2024-12-02 22:48:20 +0100

    Added tests for BarcodePriceSenderServiceTest

---
+++ src/test/java/com/dmrdmrdmr/learn/jbrains/tddintro/BarcodePriceSenderServiceTest.java
@@ -0,0 +1,9 @@
+package com.dmrdmrdmr.learn.jbrains.tddintro;
+
+class BarcodePriceSenderServiceTest {
+
+    // TODO test send message and ok response
+
+    // TODO test send message and error response
+
+}
