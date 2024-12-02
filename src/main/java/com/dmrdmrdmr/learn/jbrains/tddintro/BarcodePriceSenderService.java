8ec1a734ec6817c28825b4636de3f5e273bf32df
rbndm <rubendopico@gmail.com>
2024-12-02 23:05:05 +0100

    Implemented tests for ok/error status code

--- src/main/java/com/dmrdmrdmr/learn/jbrains/tddintro/BarcodePriceSenderService.java
+++ src/main/java/com/dmrdmrdmr/learn/jbrains/tddintro/BarcodePriceSenderService.java
@@ -1,9 +1,33 @@
 package com.dmrdmrdmr.learn.jbrains.tddintro;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class BarcodePriceSenderService {
 
-    public SendResult send(String priceMsg) {
-        return null;
+    private static final String DISPLAY_SERVICE_URL = "http://localhost:9093/display";
+    private final HttpClient httpClient;
+
+    public BarcodePriceSenderService() {
+        this.httpClient = HttpClient.newBuilder().build();
+    }
+
+    public BarcodePriceSenderService(HttpClient httpClient) {
+        this.httpClient = httpClient;
+    }
+
+    public SendResult send(String priceMsg) throws IOException, InterruptedException {
+        HttpResponse<String> response = httpClient.send(
+                HttpRequest.newBuilder()
+                        .uri(URI.create(DISPLAY_SERVICE_URL))
+                        .POST(HttpRequest.BodyPublishers.ofString("text=" + priceMsg))
+                        .build(),
+                HttpResponse.BodyHandlers.ofString());
+
+        return new SendResult(response.statusCode(), response.statusCode() == 200 ? priceMsg : null);
     }
 
     public record SendResult(int statusCode, String priceMsgSent) {}
--- src/test/java/com/dmrdmrdmr/learn/jbrains/tddintro/BarcodePriceSenderServiceTest.java
+++ src/test/java/com/dmrdmrdmr/learn/jbrains/tddintro/BarcodePriceSenderServiceTest.java
@@ -1,9 +1,40 @@
 package com.dmrdmrdmr.learn.jbrains.tddintro;
 
+import com.dmrdmrdmr.learn.jbrains.tddintro.util.MockHttpClient;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
 class BarcodePriceSenderServiceTest {
 
-    // TODO test send message and ok response
+    // test send message and ok response
+    @Test
+    void testSendAndOkResponse() throws IOException, InterruptedException {
+
+        String expectedMsg = "message";
+        int expectedStatusCode = 200;
+
+        BarcodePriceSenderService senderService = new BarcodePriceSenderService(new MockHttpClient(expectedStatusCode));
+
+        BarcodePriceSenderService.SendResult result = senderService.send(expectedMsg);
+
+        Assertions.assertEquals(expectedStatusCode, result.statusCode());
+        Assertions.assertEquals(expectedMsg, result.priceMsgSent());
+    }
 
     // TODO test send message and error response
+    @Test
+    void testSendAndErrorResponse() throws IOException, InterruptedException {
 
+        String expectedMsg = "message";
+        int expectedStatusCode = 500;
+
+        BarcodePriceSenderService senderService = new BarcodePriceSenderService(new MockHttpClient(expectedStatusCode));
+
+        BarcodePriceSenderService.SendResult result = senderService.send(expectedMsg);
+
+        Assertions.assertEquals(expectedStatusCode, result.statusCode());
+        Assertions.assertNull(result.priceMsgSent());
+    }
 }
